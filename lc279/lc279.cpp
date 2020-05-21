98eeaaa42b6a63f60e868ed586e56a1bf7c19469
dryfish118 <dryfish118@163.com>
2020-05-21 11:42:08 +0800

    lc279

--- lc279/lc279.cpp
+++ lc279/lc279.cpp
@@ -21,14 +21,70 @@
 ************************************************************************/
 
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <math.h>
+#include <iostream>
 
-int numSquares(int n) {
-	return 0;
+/*
+
+1 = 1
+2 = 1 + 1
+3 = 1 + 1 + 1
+4 = 4
+5 = 1 + 4
+6 = 1 + 1 + 4
+7 = 1 + 1 + 1 + 4
+8 = 4 + 4
+9 = 9
+10 = 1 + 9
+11 = 1 + 1 + 9
+12 = 1 + 1 + 1 + 9
+13 = 4 + 9
+14 = 1 + 4 + 9
+15 = 1 + 1 + 4 + 9
+16 = 16
+17 = 1 + 16
+18 = 1 + 1 + 16
+19 = 1 + 1 + 1 + 16
+20 = 4 + 16
+21 = 1 + 4 + 16
+22 = 1 + 1 + 4 + 16
+23 = 1 + 1 + 1 + 4 + 16
+24 = 4 + 4 + 16
+25 = 25
+26 = 1 + 25
+27 = 1 + 1 + 25
+28 = 1 + 1 + 1 + 25
+29 = 4 + 25
+30 = 1 + 4 + 25
+31 = 1 + 1 + 4 + 25
+*/
+
+void numSquares(int n)
+{
+	double kaifang = sqrt(n);
+	int quzheng = (int)kaifang;
+	int jieyu = n - quzheng * quzheng;
+	if (jieyu > 0)
+	{
+		numSquares(jieyu);
+		std::cout << " ";
+	}
+	std::cout << quzheng;
 }
 
 int main()
 {
+	std::cout << "12: ";
+	numSquares(12);
+	std::cout << std::endl;
+
+	std::cout << "13: ";
+	numSquares(13);
+	std::cout << std::endl;
+
+	std::cout << "31: ";
+	numSquares(31);
+	std::cout << std::endl;
+
 	return 0;
 }
