ce5558f953ea6886234847485854c852f92bd25a
dryfish118 <dryfish118@163.com>
2020-05-21 11:11:26 +0800

    lc49

--- lc49/lc49.cpp
+++ lc49/lc49.cpp
@@ -24,19 +24,85 @@
 ************************************************************************/
 
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
 
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
- */
-char *** groupAnagrams(char ** strs, int strsSize, int* returnSize, int** returnColumnSizes) {
-	return NULL;
+typedef std::vector<std::string> stringarray;
+typedef stringarray::iterator stringiter;
+typedef stringarray::const_iterator stringciter;
+typedef std::vector<stringarray> stringarrayarray;
+typedef stringarrayarray::iterator stringarrayiter;
+typedef std::set<char> charset;
+typedef std::map<charset, stringarray> stringarraymap;
+typedef stringarraymap::iterator stringarraymapiter;
+
+charset hashString(const std::string& str)
+{
+	charset chs;
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		chs.insert(str.at(i));
+	}
+	return chs;
+}
+
+void groupAnagrams(const stringarray& strs, stringarrayarray& rets)
+{
+	stringarraymap m;
+	for (stringciter i = strs.begin(); i != strs.end(); i++)
+	{
+		charset key = hashString(*i);
+		stringarraymapiter pos = m.find(key);
+		if (pos == m.end())
+		{
+			stringarray a;
+			a.push_back(*i);
+			m.insert(std::make_pair(key, a));
+		}
+		else
+		{
+			pos->second.push_back(*i);
+		}
+	}
+
+	for (stringarraymapiter i = m.begin(); i != m.end(); i++)
+	{
+		rets.push_back(i->second);
+	}
 }
 
 int main()
 {
+	stringarray strs;
+	strs.push_back("eat");
+	strs.push_back("tea");
+	strs.push_back("tan");
+	strs.push_back("ate");
+	strs.push_back("nat");
+	strs.push_back("bat");
+	stringarrayarray rets;
+	groupAnagrams(strs, rets);
+
+	std::cout << "[" << std::endl;
+	int count = 1;
+	for (stringarrayiter i = rets.begin(); i != rets.end(); i++)
+	{
+		stringarray& sa = *i;
+		std::cout << "  [";
+		for (stringiter j = sa.begin(); j != sa.end(); j++)
+		{
+			if (j != sa.begin())
+			{
+				std::cout << ",";
+			}
+			std::cout << "\"" << *j << "\"";
+		}
+		std::cout << "]" << std::endl;
+	}
+	std::cout << "]" << std::endl;
+	
 	return 0;
 }
